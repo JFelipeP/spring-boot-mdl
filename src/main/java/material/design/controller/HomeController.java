3c67d82dbcb7aadf4badf396de62a2da2bdb3de8
JFelipe <felipe7seven@gmail.com>
2017-01-25 17:06:44 -0200

    added Model object to home method

--- src/main/java/material/design/controller/HomeController.java
+++ src/main/java/material/design/controller/HomeController.java
@@ -1,6 +1,7 @@
 package material.design.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@GetMapping
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("message", "Material Design Lite");
 		return "home";
 	}
 
