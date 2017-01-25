bac6f9f75a062bb6ecf82188961d42be012e39c6
JFelipe <felipe7seven@gmail.com>
2017-01-25 17:42:20 -0200

    added Country Controller

---
+++ src/main/java/material/design/controller/CountryController.java
@@ -0,0 +1,28 @@
+package material.design.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import material.design.model.Country;
+import material.design.repository.CountryRepository;
+
+@Controller
+@RequestMapping("/country")
+public class CountryController {
+	
+	@Autowired
+	private CountryRepository repository;
+	
+	@GetMapping
+	public String show(Model model) {
+		List<Country> countries = repository.findAll();
+		model.addAttribute("countries", countries);
+		return "countries";
+	}
+
+}
