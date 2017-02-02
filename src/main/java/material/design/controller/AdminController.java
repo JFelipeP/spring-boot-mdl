6ca3134fb24102478de5f3007c112a5ede24edf4
JFelipe <felipe7seven@gmail.com>
2017-02-02 17:15:39 -0200

    added delete functionality for users

--- src/main/java/material/design/controller/AdminController.java
+++ src/main/java/material/design/controller/AdminController.java
@@ -1,21 +1,18 @@
 package material.design.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import material.design.exception.UsernameAlreadyInUseException;
 import material.design.model.AdminFormData;
 import material.design.model.Authorities;
 import material.design.model.Users;
-import material.design.model.UsersAndAuthorities;
 import material.design.repository.AuthoritiesRepository;
 import material.design.repository.UsersRepository;
 
@@ -31,7 +28,6 @@ public class AdminController {
 	
 	@GetMapping
 	public String showUsers(Model model) {		
-		model.addAttribute("edit", true);
 		model.addAttribute("users", usersRepository.JdbcFindUsersWithAuthorities());		
 		model.addAttribute("adminFormData", new AdminFormData(null, null, "ROLE_USER", true));
 		return "users";
@@ -49,17 +45,20 @@ public class AdminController {
 		String securePassword = bcrypt.encode(form.getPassword());
 		
 		Users userToSave = new Users(form.getUsername(), securePassword, form.isEnabled());
-		System.out.println(userToSave);
+				
 		Users usr = usersRepository.saveAndFlush(userToSave);
 		Authorities auth = authoritiesRepository.saveAndFlush(new Authorities(form.getUsername(), form.getAuthority()));
+		
 		return "redirect:/admin";
-	}
+	}	
 	
-	
-	@ResponseBody
-	@GetMapping("/showall")
-	public List<UsersAndAuthorities> showAll() {
-		return usersRepository.JdbcFindUsersWithAuthorities();
+	@GetMapping("/delete/{username}")
+	public String delete(@PathVariable String username) {
+		
+		authoritiesRepository.delete(username);
+		usersRepository.delete(username);	
+		
+		return "redirect:/admin";
 	}
 	
 	
