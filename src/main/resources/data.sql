5af18cbcb1f7b652e5ed4ca2657bf13b64b23ca4
JFelipe <felipe7seven@gmail.com>
2017-02-03 13:39:23 -0200

    corrections in data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-insert into country(name, capital, population, language, code) values ('Brazil', 'Brasila', 200000000, 'Portugues', 'BRA');
-insert into country(name, capital, population, language, code) values ('United States', 'Washington DC', 310000000, 'English', 'USA');
-insert into country(name, capital, population, language, code) values ('Japan', 'Tokyo', 150000000, 'Japanese', 'JPA');
+insert into country(name, capital, population, language, code) values ('Brazil', 'Brasila', 200000000, 'Portuguese', 'BR');
+insert into country(name, capital, population, language, code) values ('United States', 'Washington, D.C.', 310000000, 'English', 'USA');
+insert into country(name, capital, population, language, code) values ('Japan', 'Tokyo', 127000000, 'Japanese', 'JP');
 insert into country(name, capital, population, language, code) values ('France', 'Paris', 200000000, 'French', 'FR');
-insert into country(name, capital, population, language, code) values ('Italia', 'Rome', 300000000, 'Italian', 'ITA');
+insert into country(name, capital, population, language, code) values ('Italy', 'Rome', 60000000, 'Italian', 'IT');
 
 insert into users(username, password, enabled) values('user', '$2a$06$XODO9mgM4shtjA98BUhgDOachRH/pQn78TXHQvv7FNY/GWZ67FQKO', true);
 insert into authorities(username, authority) values('user', 'ROLE_USER');
